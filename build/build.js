401fa60ad349af5dcefc7be5da570dd74fd00e42
Pedro Pequeno <viniciuspedro350@gmail.com>
2019-05-06 13:12:46 -0400

    Teste novamente show lbelsby buildAndCommit - VS2019

--- build/build.js
+++ build/build.js
@@ -33,9 +33,9 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     }
 
     console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
+    console.log(chalk.red(
+      'Verifique o arquivo src/router/index.js para base do router\n' +
+      'Verfique o arquivo config/index.js tag build /padrao/'
     ))
   })
 })
