a835bc29190a43f1395f1777faab64f10cd4f609
Pedro Pequeno <viniciuspedro350@gmail.com>
2019-05-06 00:09:40 -0400

    Fisrt commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app>
+    <v-dialog v-model="modal">
+      <v-card>
+        edsf
+        <v-icon>create</v-icon>
+      </v-card>
+    </v-dialog>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        modal:true,
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
